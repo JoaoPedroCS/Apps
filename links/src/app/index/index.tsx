2863fbc90befdc1c10715c8208052077cb24e24b
JoaoPedro <joaohulck@gmail.com>
2024-10-17 13:50:38 -0300

    a

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -25,7 +25,7 @@ export default function Index() {
             <Category
                 name={categories[1].name} 
                 icon={categories[1].icon} 
-                isSelected
+                isSelected={false}
             />
         </View>
     );
