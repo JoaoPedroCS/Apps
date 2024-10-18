6591dafde502356a5eb27b540675f2cc1c4232e6
JoaoPedro <joaohulck@gmail.com>
2024-10-18 16:12:31 -0300

    updt

---
+++ links/src/components/input/index.tsx
@@ -0,0 +1,16 @@
+import { TextInput, TextInputProps } from 'react-native';
+
+import { styles } from './styles';
+import { colors } from '@/styles/colors';
+
+
+
+export function Input({ ...rest }: TextInputProps) {
+    return (
+        <TextInput
+            style={styles.container}
+            placeholderTextColor={colors.gray[400]}
+            {...rest}
+        />
+    )
+}
